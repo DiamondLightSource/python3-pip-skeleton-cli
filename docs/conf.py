06a2b43531bfd497601b3998bc959a6974f1ca46
Tom C (DLS) <101418278+coretl@users.noreply.github.com>
2022-09-13 09:36:56 +0100

    Correct docs pypi link

--- docs/conf.py
+++ docs/conf.py
@@ -138,7 +138,7 @@ html_theme_options = dict(
     icon_links=[
         dict(
             name="PyPI",
-            url=f"https://pypi.org/project/{project}",
+            url=f"https://pypi.org/project/python3-pip-skeleton",
             icon="fas fa-cube",
         )
     ],
