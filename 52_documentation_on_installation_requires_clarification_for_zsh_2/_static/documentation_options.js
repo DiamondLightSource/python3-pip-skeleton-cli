5a26eab51a02ced1f08658ec34ed6c8f4ffcf2ca
evalott100 <evalott100@users.noreply.github.com>
2023-01-26 15:32:21 +0000

    deploy: 4b3b5402800dd0feda34a8b8c2e6697d11105246

--- 52_documentation_on_installation_requires_clarification_for_zsh_2/_static/documentation_options.js
+++ 52_documentation_on_installation_requires_clarification_for_zsh_2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.1.dev45+gfe32919',
+    VERSION: '3.3.1.dev47+g4b3b540',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
