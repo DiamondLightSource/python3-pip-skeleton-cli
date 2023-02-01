77418f307c3d574776256a81dde819428e786d08
evalott100 <evalott100@users.noreply.github.com>
2023-02-01 11:47:45 +0000

    deploy: 6d09bbd2e46404f6976021b3c3cce3d275000d69

--- 45_existing_command_should_be_best_effort/_static/documentation_options.js
+++ 45_existing_command_should_be_best_effort/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.1.dev50+gaff8789',
+    VERSION: '3.3.1.dev4+g6d09bbd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
