b11e7200e53071d4eaa4f60f2ade8fc0e6cb4f0d
evalott100 <evalott100@users.noreply.github.com>
2023-02-22 11:22:55 +0000

    deploy: 994cba37dcc54572939fd8c5b16b68f87868aa0f

--- 73_allow_for_use_with_skeletons_other_than_diamond/_static/documentation_options.js
+++ 73_allow_for_use_with_skeletons_other_than_diamond/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.1.dev59+g344e4f7',
+    VERSION: '3.3.1.dev59+g994cba3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
