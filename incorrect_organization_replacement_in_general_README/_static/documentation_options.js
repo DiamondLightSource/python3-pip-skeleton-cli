9072c7a1edbf52796632d731b333ab7b4332b7d3
evalott100 <evalott100@users.noreply.github.com>
2023-04-12 14:50:47 +0000

    deploy: 14a16cbcb199c3ff0687bab57aa5dcc4af6a8e8a

--- incorrect_organization_replacement_in_general_README/_static/documentation_options.js
+++ incorrect_organization_replacement_in_general_README/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.3.1.dev59+g7d0828c',
+    VERSION: '3.3.1.dev59+g14a16cb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
