04d2602fe32f5e882633e176c2e75ace2c12a28e
evalott100 <evalott100@users.noreply.github.com>
2023-05-15 12:39:28 +0000

    deploy: 3a71f476aa4df7b896288a1dc0969c522dfe12ac

--- need_to_substitute_this_line_in_the_cli/_static/documentation_options.js
+++ need_to_substitute_this_line_in_the_cli/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.1.dev8+g1f94814',
+    VERSION: '3.4.1.dev8+g3a71f47',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
