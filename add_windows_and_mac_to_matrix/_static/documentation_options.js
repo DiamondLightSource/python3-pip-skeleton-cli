3b1912be65cf683649e99cf8f1f0e429e2fc7d1d
evalott100 <evalott100@users.noreply.github.com>
2023-05-24 07:49:44 +0000

    deploy: 6e2840f469eab1971e915cccc23fbae8f42fa1b4

--- add_windows_and_mac_to_matrix/_static/documentation_options.js
+++ add_windows_and_mac_to_matrix/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.1.dev11+g24187c6',
+    VERSION: '3.4.1.dev11+g6e2840f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
